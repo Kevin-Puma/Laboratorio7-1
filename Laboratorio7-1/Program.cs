b16937e2bb071c55cb3369d4634b0c85058a09d7
User <User@DESKTOP-CRJD420>
2023-10-04 19:22:54 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7-1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese el valor de n: ");
+float n = Convert.ToSingle(Console.ReadLine());
+float suma = 0;
+
+for (float i = 1; i <= n; i++)
+{
+    float termino = ((16 - i) / (3 * i)); // Calcula cada término de la secuencia
+    suma += termino;
+    Console.WriteLine("Resultado : " + termino + "");
+}
+Console.WriteLine("La suma es : " + suma + "");
